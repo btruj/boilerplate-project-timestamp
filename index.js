c49cbbe12d675f484aa12f1d74d066269326a35b
Your Name <Your Email>
2022-11-02 18:32:16 -0400

    changed to port 5000 & added unix & ut Date

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +19,19 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get('/api', (req, res) => {
+res.json({
+unix: new Date().getTime(),
+utc: new Date().toUTCString(),
+});
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(5000, function () {
+  console.log('Your app is listening on port ' + 5000);
 });
