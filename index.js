729b45827dc79feb8678d761ac1c4f145e6f17e2
Your Name <Your Email>
2022-11-02 20:36:56 -0400

    Deploy to Heroku

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ return res.json({
 })
 
 // listen for requests :)
-var listener = app.listen(server.port, function () {
-  console.log('Your app is listening on port ' + server.port);
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
